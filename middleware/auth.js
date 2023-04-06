603b1af6fe738d56a52d2a336f3b14a74a1eb909
Ashutosh Deshmukh <ashutoshdeshmukhh@gmail.com>
2023-04-06 15:59:20 +0530

    final

---
+++ middleware/auth.js
@@ -0,0 +1,22 @@
+const jwt = require('jsonwebtoken');
+
+const config = process.env;
+
+const verifyToken = (req, res, next) => {
+  const token =
+    req.body.token || req.query.token || req.headers['authorization'];
+
+  if (!token) {
+    return res.status(403).send('A token is required for authentication');
+  }
+  try {
+    const decoded = jwt.verify(token, config.TOKEN_KEY);
+    req.user = decoded;
+    console.log('Decoded Token -' + decoded);
+  } catch (err) {
+    return res.status(401).send('Invalid Token');
+  }
+  return next();
+};
+
+module.exports = verifyToken;
